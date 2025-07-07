ce247914560c523cf7e2fbef735e0c3669623249
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-07 18:21:55 +0500

    fix: replace psycopg2 with psycopg2-binary

--- core/core/settings.py
+++ core/core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f_ahw@tm!i07*#cw*898kh2a$+%vam&gc)+*+rg)*#g#ol41e5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]  
 
