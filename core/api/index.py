8e709b683a6d8406eb793cdd28c8e22fc420fb82
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-07 18:05:29 +0500

    fix: replace psycopg2 with psycopg2-binary

--- core/api/index.py
+++ core/api/index.py
@@ -1,5 +1,10 @@
-from core.asgi import application as app  # Vercel looks for 'app'
+import os
+import django
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from core.asgi import application as app  # 👈 Vercel needs `app` or `handler`
 
 handlers = {
     "http": app,
