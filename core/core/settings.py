c9279d1174b57f414ffeabb5f941788969e80638
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-08 11:18:19 +0500

    Add domain urls

--- core/core/settings.py
+++ core/core/settings.py
@@ -154,7 +154,8 @@ AUTH_USER_MODEL = 'accounts.User'
 
 CORS_ALLOWED_ORIGINS = [
     "https://chat-app1-phi.vercel.app/",
-    "https://backend-seven-amber-92.vercel.app/"
+    "https://backend-seven-amber-92.vercel.app/",
+    "https://backend-production-5b2b.up.railway.app/"
     
 ]
 
