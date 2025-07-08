cccd6f44426164e65372497641e287392025915b
ATTA UR REHMAN <manirana770@gmail.com>
2025-07-08 11:49:14 +0500

    Fix CORS issue

--- core/core/settings.py
+++ core/core/settings.py
@@ -46,13 +46,14 @@ INSTALLED_APPS = [
     'channels_redis',
     'accounts', 
     'chat',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -160,6 +161,7 @@ CORS_ALLOWED_ORIGINS = [
     
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
